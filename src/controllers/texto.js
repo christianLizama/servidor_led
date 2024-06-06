1f2604688b102a2a18b9b38e5df9ff5f57697aa9
christianLizama <ckill661@gmail.com>
2024-06-05 20:48:22 -0400

    Se envía un mensaje de texto por la ruta hacia el tablero

--- src/controllers/texto.js
+++ src/controllers/texto.js
@@ -1,54 +1,71 @@
-import Texto from '../models/texto.js';
-
+import Texto from "../models/texto.js";
+import { sendMessageToESP32 } from "../services/messageSender.js";
 //agregar texto
 export const agregarTexto = async (req, res) => {
-    try {
-        const texto = new Texto(req.body);
-        await texto.save();
-        res.status(201).json(texto);
-    } catch (error) {
-        res.status(500).json({ mensaje: error.message });
-    }
-}
+  try {
+    const texto = new Texto(req.body);
+    await texto.save();
+    res.status(201).json(texto);
+  } catch (error) {
+    res.status(500).json({ mensaje: error.message });
+  }
+};
 
 //obtener textos
 export const obtenerTextos = async (req, res) => {
-    try {
-        const texto = await Texto.find();
-        res.status(200).json(texto);
-    } catch (error) {
-        res.status(500).json({ mensaje: error.message });
-    }
-}
+  try {
+    const texto = await Texto.find();
+    res.status(200).json(texto);
+  } catch (error) {
+    res.status(500).json({ mensaje: error.message });
+  }
+};
 
 //obtener texto por id
 export const obtenerTextoPorId = async (req, res) => {
-    try {
-        const texto = await Texto.findById(req.params.id);
-        res.status(200).json(texto);
-    } catch (error) {
-        res.status(500).json({ mensaje: error.message });
-    }
-}
+  try {
+    const texto = await Texto.findById(req.params.id);
+    res.status(200).json(texto);
+  } catch (error) {
+    res.status(500).json({ mensaje: error.message });
+  }
+};
 
 //actualizar un texto
 export const actualizarTexto = async (req, res) => {
-    try {
-        const texto = await Texto.findByIdAndUpdate(req
-            .params.id, req.body, { new: true });
-        res.status(200).json(texto);
-    }
-    catch (error) {
-        res.status(500).json({ mensaje: error.message });
-    }
-}
+  try {
+    const texto = await Texto.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.status(200).json(texto);
+  } catch (error) {
+    res.status(500).json({ mensaje: error.message });
+  }
+};
 
 //eliminar un texto
 export const eliminarTexto = async (req, res) => {
-    try {
-        await Texto.findByIdAndDelete(req.params.id);
-        res.status(200).json({ mensaje: "Texto eliminado" });
-    } catch (error) {
-        res.status(500).json({ mensaje: error.message });
-    }
-}
+  try {
+    await Texto.findByIdAndDelete(req.params.id);
+    res.status(200).json({ mensaje: "Texto eliminado" });
+  } catch (error) {
+    res.status(500).json({ mensaje: error.message });
+  }
+};
+
+export const enviarTexto = async (req, res) => {
+  try {
+    // Extrae el mensaje de la solicitud HTTP
+    const message = req.body.message;
+
+    // Envía el mensaje al ESP32
+    sendMessageToESP32(message);
+
+    // Envía una respuesta HTTP de éxito
+    res
+      .status(200)
+      .json({ success: true, message: "Mensaje enviado al ESP32." });
+  } catch (error) {
+    res.status(500).json({ mensaje: error.message });
+  }
+};
--- src/routes/texto.js
+++ src/routes/texto.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { agregarTexto, obtenerTextos, obtenerTextoPorId, actualizarTexto, eliminarTexto } from "../controllers/texto.js";
+import { agregarTexto, obtenerTextos, obtenerTextoPorId, actualizarTexto, eliminarTexto,enviarTexto } from "../controllers/texto.js";
 
 const router = express.Router();
 
@@ -8,5 +8,6 @@ router.get("/getAll", obtenerTextos);
 router.get("/get/:id", obtenerTextoPorId);
 router.put("/update/:id", actualizarTexto);
 router.delete("/delete/:id", eliminarTexto);
+router.post("/send", enviarTexto);
 
 export default router;
